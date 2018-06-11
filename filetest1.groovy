21253478bd194997c0a31d0b111b38f2fb1d5ee6
Ivica Loncar <ilx@users.noreply.github.com>
2018-06-11 15:49:41 +0200

    already exists

---
+++ filetest1.groovy
@@ -0,0 +1,20 @@
+// Exported from:        http://xenotron.local:5516/#/templates/Folderd5db69e1970c4736a8d6605f9fbc1722-Release9e010ce3b3ed4317aa345de043c23fab/releasefile
+// XL Release version:   0.0.0-SNAPSHOT
+// Date created:         Mon Jun 11 15:49:40 CEST 2018
+
+xlr {
+  template('fdsafda') {
+    folder('ilx')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-06-11T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('vfds') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
