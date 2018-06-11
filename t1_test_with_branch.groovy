1914c75af68d5d15ab83813c1677cfd45d083ee5
Ivica Loncar <ilx@users.noreply.github.com>
2018-06-11 14:44:21 +0200

    dsfdsgfds

--- t1_test_with_branch.groovy
+++ t1_test_with_branch.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://xenotron.local:5516/#/templates/Folder8f962ccf46244b98992cee417999117b-Release50e8b368ab69417686608e7df9ab9f0c/releasefile
 // XL Release version:   0.0.0-SNAPSHOT
-// Date created:         Mon Jun 11 14:43:51 CEST 2018
+// Date created:         Mon Jun 11 14:44:20 CEST 2018
 
 xlr {
   template('t1') {
