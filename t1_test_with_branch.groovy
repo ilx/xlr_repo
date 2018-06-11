43bb7c7826793a8686869e3ab1637982cd66a203
Ivica Loncar <ilx@users.noreply.github.com>
2018-06-11 14:43:52 +0200

    gdsfdsafd

---
+++ t1_test_with_branch.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://xenotron.local:5516/#/templates/Folder8f962ccf46244b98992cee417999117b-Release50e8b368ab69417686608e7df9ab9f0c/releasefile
+// XL Release version:   0.0.0-SNAPSHOT
+// Date created:         Mon Jun 11 14:43:51 CEST 2018
+
+xlr {
+  template('t1') {
+    folder('ilx')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-06-11T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
