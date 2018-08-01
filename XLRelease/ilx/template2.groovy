e2fe1aa75676d069e7f77a8fca017db81f414230
Ivica Loncar <ilx@users.noreply.github.com>
2018-08-01 17:22:02 +0200

    v1.0.106

---
+++ XLRelease/ilx/template2.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://xenotron.home:5516/#/templates/Folderfc6c80da2fa74fda86b7b1fa063b2fbf-Release7c7aa3529ab147fdad605d015834ccef/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Wed Aug 01 17:22:01 CEST 2018
+
+xlr {
+  template('template2') {
+    folder('ilx')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-01T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
