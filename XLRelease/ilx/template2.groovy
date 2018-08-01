eeebfe5b2757fdb4b8536163e411c3c590f1cd08
Ivica Loncar <ilx@users.noreply.github.com>
2018-08-01 18:24:09 +0200

    v1.0.107

--- XLRelease/ilx/template2.groovy
+++ XLRelease/ilx/template2.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://xenotron.home:5516/#/templates/Folderfc6c80da2fa74fda86b7b1fa063b2fbf-Release7c7aa3529ab147fdad605d015834ccef/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Wed Aug 01 17:22:01 CEST 2018
+// Date created:         Wed Aug 01 18:24:08 CEST 2018
 
 xlr {
   template('template2') {
