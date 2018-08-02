dad9d32a2680792d4d31b4bac256a98849798b0d
Ivica Loncar <ilx@users.noreply.github.com>
2018-08-02 12:36:23 +0200

    v1.0.1

--- XLRelease/folder1/T1.groovy
+++ XLRelease/folder1/T1.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://xenotron.local:5516/#/templates/Folder5aa3aa2da8044d3092a566e910d61282-Released0552e7990344249ac804a8fda0cf1b/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Thu Aug 02 12:23:37 CEST 2018
+// Date created:         Thu Aug 02 12:36:22 CEST 2018
 
 xlr {
   template('T1') {
