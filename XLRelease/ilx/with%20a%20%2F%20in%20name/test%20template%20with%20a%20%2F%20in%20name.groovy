6692b3d431c6d55b31fddbc84b1bffa15e122e96
Ivica Loncar <ilx@users.noreply.github.com>
2018-08-02 18:25:42 +0200

    v1.0.201

---
+++ XLRelease/ilx/with%20a%20%2F%20in%20name/test%20template%20with%20a%20%2F%20in%20name.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://xenotron.local:5516/#/templates/Folderfc6c80da2fa74fda86b7b1fa063b2fbf-Folder1cfb79b968da472bbf8203dca57ab844-Release24a36be32bea4c89bf8f72653bfa8349/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Thu Aug 02 18:25:41 CEST 2018
+
+xlr {
+  template('test template with a / in name') {
+    folder('ilx/with a / in name')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-02T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
