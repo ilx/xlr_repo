00d6af50e47b85b9156d99300cf3c363c2b8c494
Ivica Loncar <ilx@users.noreply.github.com>
2018-08-06 17:08:17 +0200

    v1.0.202

---
+++ XLRelease/ilx/with a in name/test template with a slash in name.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://xenotron.Home:5516/#/templates/Folderfc6c80da2fa74fda86b7b1fa063b2fbf-Folder1cfb79b968da472bbf8203dca57ab844-Release24a36be32bea4c89bf8f72653bfa8349/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Mon Aug 06 17:08:15 CEST 2018
+
+xlr {
+  template('test template with a slash in name') {
+    folder('ilx/with a in name')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-02T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
