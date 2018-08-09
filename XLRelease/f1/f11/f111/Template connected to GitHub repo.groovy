0adecc72e0b9522cd98fdd63c29fce44983fa2d2
Ivica Loncar <ilx@users.noreply.github.com>
2018-08-09 14:50:44 +0200

    v0.0.1 of Template connected to GitHub repo

---
+++ XLRelease/f1/f11/f111/Template connected to GitHub repo.groovy
@@ -0,0 +1,20 @@
+// Exported from:        http://xenotron.Home:5516/#/templates/Folder76139267e3f94ad6bfa052553438edc5-Folder245535d3838d4848b70f0f84f62417c9-Folderf01f5e80942d46369649974b73fb3377-Release472e1e6b53644330888eb86f050b82d5/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Thu Aug 09 14:50:44 CEST 2018
+
+xlr {
+  template('Template connected to GitHub repo') {
+    folder('f1/f11/f111')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-09T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('m1') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
